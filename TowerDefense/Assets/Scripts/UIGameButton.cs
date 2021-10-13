b33961cde5f41cc8e8b894b24d29cb85053f010c
Misha <myhaylo03@gmail.com>
2021-10-13 20:59:56 +0300

    made visual part of game scene

---
+++ TowerDefense/Assets/Scripts/UIGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UIGameButton : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private UnityEvent MouseClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        MouseClick?.Invoke();
+    }
+}
