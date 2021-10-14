8b781067f48fffa95470fb7ffe2c92fe41d09917
Misha <myhaylo03@gmail.com>
2021-10-15 01:19:19 +0300

    added build menu

---
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _buildPlace;
+    [SerializeField] private List<GameObject> _listOfBuildOptions;
+
+    private GameObject _currentBuilding;
+    private GameObject _building;
+
+
+    public void CheckConfirmButtonState(BuildOptionProperties buildOptionProperties)
+    {
+        _buildPlace.SetActive(false);
+        ShowBuilding(buildOptionProperties);
+
+        if (!buildOptionProperties.ConfirmIcon.activeSelf)
+        {
+            ShowOrHideConfirmButton(buildOptionProperties, true);
+
+            for (int i = 0; i < _listOfBuildOptions.Count; i++)
+            {
+                var buildOP = _listOfBuildOptions[i].GetComponent<BuildOptionProperties>();
+                if (buildOP != buildOptionProperties)
+                {
+                    ShowOrHideConfirmButton(buildOP, false);
+                }
+            }
+        }
+        else
+        {
+            _listOfBuildOptions[0].transform.parent.gameObject.SetActive(false);
+            //money--
+        }
+    }
+
+
+    private void ShowOrHideConfirmButton(BuildOptionProperties buildOptionProperties, bool show)
+    {
+        foreach (Transform childTransform in buildOptionProperties.gameObject.GetComponentInChildren<Transform>())
+        {
+            childTransform.gameObject.SetActive(!show);
+        }
+
+        buildOptionProperties.ConfirmIcon.SetActive(show);
+    }
+
+
+    private void ShowBuilding(BuildOptionProperties buildOptionProperties)
+    {
+        if(_building != null)
+        {
+            Destroy(_building);
+        }
+        _currentBuilding = Resources.Load("BuildingPrefabs/" + buildOptionProperties.NameOfBuilding) as GameObject;
+
+        var localTransform = _currentBuilding.transform;
+        _building = Instantiate(_currentBuilding, _currentBuilding.transform.position, Quaternion.identity, gameObject.transform);
+
+        _building.transform.SetParent(gameObject.transform);
+        _building.transform.localPosition = localTransform.position;
+        _building.transform.localScale = localTransform.localScale;
+    }
+}
---
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _listOfBuildMenus;
+
+
+    private static GameManager _instance;
+
+    public static GameManager Instance()
+    {
+        if (_instance == null)
+            _instance = new GameManager();
+        return _instance;
+    }
+
+
+    public void OpenBuildMenu(GameObject buildMenu)
+    {
+        for (int i = 0; i < _listOfBuildMenus.Count; i++)
+        {
+            if (_listOfBuildMenus[i].activeSelf)
+            {
+                _listOfBuildMenus[i].SetActive(false);
+            }
+        }
+
+        buildMenu.SetActive(true);
+    }
+}
