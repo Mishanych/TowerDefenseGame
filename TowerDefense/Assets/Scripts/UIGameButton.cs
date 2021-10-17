2e67903e722811a2e55911a376441bc089f32cef
Misha <myhaylo03@gmail.com>
2021-10-17 17:43:21 +0300

    fixed game button

--- TowerDefense/Assets/Scripts/UIGameButton.cs
+++ TowerDefense/Assets/Scripts/UIGameButton.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIGameButton : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private UnityEvent MouseClick;
 
+    [SerializeField] bool _enabled = true;
+
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        MouseClick?.Invoke();
+        if(_enabled)
+            MouseClick?.Invoke();
+    }
+
+    public void SwitchEnable()
+    {
+        if (_enabled)
+            DisableButton();
+        else
+            EnableButton();
+    }
+
+    public void DisableButton()
+    {
+        _enabled = false;
+        foreach (Image btnImage in gameObject.GetComponentsInChildren<Image>())
+        {
+            if (btnImage != null)
+            {
+                var color = btnImage.color;
+                color.a = 0.5f;
+                btnImage.color = color;
+            }
+        }
+    }
+
+    public void EnableButton()
+    {
+        _enabled = true;
+        foreach (Image btnImage in gameObject.GetComponentsInChildren<Image>())
+        {
+            if (btnImage != null)
+            {
+                var color = btnImage.color;
+                color.a = 1f;
+                btnImage.color = color;
+            }
+        }
     }
 }
