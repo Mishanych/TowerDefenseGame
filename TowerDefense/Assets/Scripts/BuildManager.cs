b30213c085c30f02e521cd603a5fa46db5217bd3
Misha <myhaylo03@gmail.com>
2021-10-17 17:44:13 +0300

    added player data

--- TowerDefense/Assets/Scripts/BuildManager.cs
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -9,13 +9,17 @@ public class BuildManager : MonoBehaviour
     [SerializeField] public GameObject BuildPlace;
     [SerializeField] public GameObject Tower;
     [SerializeField] private GameObject _buildMenu;
-    [SerializeField] private GameObject _extraMenu;
-
+    
+    public GameObject ExtraMenu;
     public List<GameObject> ListOfBuildOptions;
+
     [HideInInspector] public TowerData CurrentTowerData;
+    [HideInInspector] public bool TowerIsBuilded = false;
 
-    public bool TowerIsBuilded = false;
 
+    private Transform _target;
+    private GameObject _nearestEnemy;
+    private GameObject _ownProjectile;
 
     public void CheckBuildOptionIcons(BuildOptionSettings currentOption)
     {
@@ -29,29 +33,60 @@ public class BuildManager : MonoBehaviour
         }
     }
 
-    public void OpenExtraMenu()
-    {
-        if (!_buildMenu.activeSelf)
-        {
-            _extraMenu.SetActive(true);
-            var tmPro = _extraMenu.GetComponentInChildren<TextMeshProUGUI>();
-            tmPro.text = (CurrentTowerData.BuildPrice / 2).ToString();
-        }
-    }
-
     public void SellTower()
     {
         if (TowerIsBuilded)
         {
             Tower.GetComponent<Image>().enabled = false;
-            _extraMenu.SetActive(false);
+            ExtraMenu.SetActive(false);
             BuildPlace.SetActive(true);
+            
+            TowerIsBuilded = false;
+            GameManager.Instance.MoneyAmount += CurrentTowerData.BuildPrice;
+            Debug.LogWarning(GameManager.Instance.MoneyAmount);
             foreach (var option in ListOfBuildOptions)
             {
                 option.GetComponent<BuildOptionSettings>().SetBuildOptions();
             }
-            TowerIsBuilded = false;
-            //money++
+        }
+    }
+    private void Update()
+    {
+        UpdateTarget();
+        ShootAtTarget();
+
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float shortestDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                _nearestEnemy = enemy;
+            }
+        }
+
+        if (_nearestEnemy != null && shortestDistance <= CurrentTowerData.Range)
+        {
+            _target = _nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+    }
+
+    private void ShootAtTarget()
+    {
+        if(_target != null)
+        {
+            //var projectile = Instantiate()
         }
     }
     
---
+++ TowerDefense/Assets/Scripts/ScriptableObjects/PlayerData/PlayerData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PlayerData", menuName = "Player Data", order = 51)]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField] private int _healthAmount;
+    [SerializeField] private int _moneyAmount;
+    [SerializeField] private int _enemyWaves;
+
+
+    public int HealthAmount
+    {
+        get
+        {
+            return _healthAmount;
+        }
+    }
+
+    public int MoneyAmount
+    {
+        get
+        {
+            return _moneyAmount;
+        }
+    }
+
+    public int EnemyWaves
+    {
+        get
+        {
+            return _enemyWaves;
+        }
+    }
+}
