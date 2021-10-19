45752a1325f549316f5994d9bf2ce046118a874f
Misha <myhaylo03@gmail.com>
2021-10-19 23:47:04 +0300

    added start screen

---
+++ TowerDefense/Assets/Scripts/GameManageButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManageButtons : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    public void StartGame()
+    {
+        _animator.SetBool("StartFading", false);
+        Debug.LogWarning("PLAY");
+        StartCoroutine(Loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        _animator.SetBool("StartFading", true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
